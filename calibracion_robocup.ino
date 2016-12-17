627759bdc0d8937627c095bde495c0d1a4eeec90
patriciowelch <patriciowelch@gmail.com>
2016-12-17 17:26:06 -0300

    Update calibracion_robocup.ino

--- calibracion_robocup.ino
+++ calibracion_robocup.ino
@@ -12,8 +12,18 @@ void setup()
 
 void loop()
 {
-  s0=analogRead(3);
+  s0=analogRead(0);
+  s1=analogRead(1);
+  s2=analogRead(2);
+  s3=analogRead(3);
+  s4=analogRead(4);
+  s5=analogRead(5);
   Serial.println(s0);
+  Serial.println(s1);
+  Serial.println(s2);
+  Serial.println(s3);
+  Serial.println(s4);
+  Serial.println(s5);
   Serial.println("  ");
   delay(500);
 }
