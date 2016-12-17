1a7ce03d8f3a12faefbae18287458e6053c4fed6
patriciowelch <patriciowelch@gmail.com>
2016-12-17 17:24:44 -0300

    los codigos que use
    el código de desafio robocup lo inicie pero al no tener el robot a mano después no funciono nada y lo borre e inicie desde 0

---
+++ calibracion_robocup.ino
@@ -0,0 +1,21 @@
+int s0;
+int s1;
+int s2;
+int s3;
+int s4;
+int s5;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  s0=analogRead(3);
+  Serial.println(s0);
+  Serial.println("  ");
+  delay(500);
+}
+
+
---
+++ desafio_robocup.ino
@@ -0,0 +1,54 @@
+#include <DCMotor.h>
+
+DCMotor motor_izq(M0_EN, M0_D0, M0_D1);
+DCMotor motor_der(M1_EN, M1_D0, M1_D1);
+
+#define seguidor 1
+#define frenar 2
+
+int s_der_e;
+int s_der_i;
+int s_izq_i;
+int s_izq_e;
+
+int estado = seguidor;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  s_der_e = analogRead(3);
+  s_der_i = analogRead(4);
+  s_izq_i = analogRead(1);
+  s_izq_e = analogRead(5);
+  
+  if(estado == seguidor)
+  {
+    if(s_der_i > 25 && s_izq_i > 25)
+    {
+      motor(-5,-5);
+      estado = frenar;
+    }
+    else if(s_der_e > 25)
+    {
+      motor(35,50);
+    }
+    else if(s_izq_e > 25)
+    {
+      motor(50,35);
+    }
+    else
+    {
+      motor(40,40);
+    }
+  }
+}
+
+void motor(int vel1, int vel2)
+{
+  motor_izq.setSpeed(vel1);
+  motor_der.setSpeed(vel2);
+}
